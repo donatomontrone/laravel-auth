30503a1c6dbdc903ff395bd36cb910f520accc3b
donatomontrone <donato.montrone@outlook.com>
2023-02-22 12:29:39 +0100

    added for to select

--- resources/views/admin/projects/partials/form.blade.php
+++ resources/views/admin/projects/partials/form.blade.php
@@ -19,11 +19,13 @@
         <div class="col-md">
             <div class="form-floating">
                 <select name="complexity" id="complexitySelect" class="form-select">
-                    <option value="1" {{ old('complexity', $project->complexity) == '1' ? 'selected' : '' }}>1</option>
-                    <option value="2" {{ old('complexity', $project->complexity) == '2' ? 'selected' : '' }}>2</option>
+                    @for ($i = 1; $i < 6; $i++)
+                    <option value="{{$i}}" {{ old('complexity', $project->complexity) == "$i" ? 'selected' : '' }}>{{$i}}</option>
+                    @endfor
+                    {{-- <option value="2" {{ old('complexity', $project->complexity) == '2' ? 'selected' : '' }}>2</option>
                     <option value="3" {{ old('complexity', $project->complexity) == '3' ? 'selected' : '' }}>3</option>
                     <option value="4" {{ old('complexity', $project->complexity) == '4' ? 'selected' : '' }}>4</option>
-                    <option value="5" {{ old('complexity', $project->complexity) == '5' ? 'selected' : '' }}>5</option>
+                    <option value="5" {{ old('complexity', $project->complexity) == '5' ? 'selected' : '' }}>5</option> --}}
                 </select>
                 <label for="complexitySelect">Project Complexity</label>
             </div>
