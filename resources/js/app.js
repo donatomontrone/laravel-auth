de8daa228875eb8e9a991ec3d090a7734973449b
donatomontrone <donato.montrone@outlook.com>
2023-02-23 14:54:12 +0100

    added dark theme on popUp

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
